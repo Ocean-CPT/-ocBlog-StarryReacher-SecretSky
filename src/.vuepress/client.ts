074dcc270a049bf13c0d0ef6219aa4a9e1716db2
陈皮糖CPT <138201271+Ocean-CPT@users.noreply.github.com>
2025-01-11 19:45:04 +0800

    feat: transparent navbar

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,16 @@
+import { defineClientConfig } from "vuepress/client";
+import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
+import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
+
+export default defineClientConfig({
+    setup() {
+        setupRunningTimeFooter(
+            new Date("2025-01-11"),
+            {
+                "/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
+            },
+            true,
+        );
+        setupTransparentNavbar();
+    },
+});
